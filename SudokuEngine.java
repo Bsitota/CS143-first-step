17c2aab9beb1edb539f403be43fb165c2ab5b445
Bsitota <biruk.sitota@go.shoreline.edu>
2025-04-20 23:20:35 -0700

    main class code to test or run the sudoku board

---
+++ SudokuEngine.java
@@ -0,0 +1,9 @@
+public class SudokuEngine {
+    public static void main(String[] args) {
+        SudokuBoard myBoard = new SudokuBoard("/Users/bsitota/Desktop/Java || cs 143/Sudoku/data1.sdk");
+        SudokuBoard myBoard2 = new SudokuBoard("/Users/bsitota/Desktop/Java || cs 143/Sudoku/data1.sdk");
+        System.out.println(myBoard);
+        System.out.println();
+        System.out.println(myBoard2);
+    }
+}
